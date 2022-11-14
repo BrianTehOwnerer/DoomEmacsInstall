72717355854991e668db02ec55e529b0a85d808b
brian <brian@mettenbrink.org>
2022-11-14 14:09:02 -0600

    more updates. you know the drill

--- installEmacs.ps1
+++ installEmacs.ps1
@@ -1,25 +1,37 @@
 ## to do task list.
-##install chocolaty
-##install emacs ripgrep fd git
-##set $PATH veriables 
+##install chocolaty             ✓
+##install emacs ripgrep fd git  ✓
+##set $PATH veriables           ✓
 ## run doom install script
 ## set up emacs server
 ## add shortcut to desktop for client
+## Install all-the-icons font  https://github.com/domtronn/all-the-icons.el/tree/master/fonts
 
 
 
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install git ripgrep fd git -y 
+function SetEnvironmentalVeriables {
+    ##This bit of code writes the new $PATH value to the registry... yes windows is this stupid you have to read the old value, add yours to it, and write it back whole hog.
+    $LoggedInUser = $env:UserName
 
+    $NewPathData = "C:\Users\" + $LoggedInUser + "\.emacs.d\bin" + ":" + "C:\Users\" + $LoggedInUser + "\.emacs.d\bin"
+    #Set registry key
+    $RegistryPath = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+    #pull current registry key data
+    $OldPathData = (Get-ItemProperty -Path "$RegistryPath" -Name PATH).Path
+    #create the final string
+    $FinalPathData = $OldPathData + ":" + $NewPathData
+    #write string back to registry for PATH
+    Set-ItemProperty -Path "$RegistryPath" -Name PATH -Value $FinalPathData
 
-##This bit of code writes the new $PATH value to the registry... yes windows is this stupid you have to read the old value, add yours to it, and write it back whole hog.
-$LoggedInUser = $env:UserName
-$NewPathData = "C:\Users\"+ $LoggedInUser +"\.emacs.d\bin" +":"+ "C:\Users\"+ $LoggedInUser +"\.emacs.d\bin"
-$RegistryPath = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
-$OldPathData = (Get-ItemProperty -Path "$RegistryPath" -Name PATH).Path
-$FinalPathData = $OldPathData + ":" + $NewPathData
-Set-ItemProperty -Path "$RegistryPath" -Name PATH -Value $FinalPathData
+    #Create the "HOME" path for emacs, so it uses that insted of the appdata folder.
+    $EnvVeriableHOMEData = "C:\Users\" + $LoggedInUser + "\"
+    Set-ItemProperty -Path "$RegistryPath" -Name HOME -Value $EnvVeriableHOMEData
+}
 
-$EnvVeriableHOMEData = "C:\Users\"+$LoggedInUser+"\"
-Set-ItemProperty -Path "$RegistryPath" -Name HOME -Value $EnvVeriableHOMEData
+functon installchco {
+    #Installs chocolatey and requried software
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    choco install git ripgrep fd git -y 
+    choco install git ripgrep fd git -y 
+}
